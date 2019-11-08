cb14b9c5dbdbc7cab23610bbdc9790fe821e9c94
Khushbu Nakum <nakumk03@gmail.com>
2019-11-08 16:54:12 -0500

    changes

--- EventGo/EventGo/HomeViewController.swift
+++ EventGo/EventGo/HomeViewController.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import UIKit
+import MapKit
+import GooglePlaces
+
+class HomeViewController: UIViewController{
+    
+    var results: GMSAutocompleteResultsViewController!
+    var searchController: UISearchController!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func setsearchController(){
+        results = GMSAutocompleteResultsViewController()
+        searchController = UISearchController(searchResultsController: results)
+        searchController?.searchResultsUpdater = results
+        
+        let searchBar = searchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for Places"
+        navigationItem.titleView = searchController.searchBar
+        definesPresentationContext = true
+    
+        searchController?.hidesNavigationBarDuringPresentation = false
+        
+    }
+
+
+}
