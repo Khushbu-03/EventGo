5b2f3adaa77f4ee65b46e832ef086224b4a67fd4
Ankit <luthraan@msu.edu>
2019-11-08 19:04:38 -0500

    Added login page

--- EventGo/EventGo/SignUpViewController.swift
+++ EventGo/EventGo/SignUpViewController.swift
@@ -23,6 +23,8 @@ class SignUpViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
     @IBOutlet weak var signUpButton: UIButton!
     
     @IBOutlet weak var errorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
